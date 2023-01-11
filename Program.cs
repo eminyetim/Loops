3c4aae1e1a5a92fecde7252bf90e5a284a584927
emin <eminyetim25@gmail.com>
2023-01-12 01:27:31 +0300

    second

--- Program.cs
+++ Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("First Commit Loops");
+        // Console.Write("Please enter the a number :");
+        // int counter = int.Parse(Console.ReadLine());
+       
+
+        // for (int i = 0; i < counter; i++)
+        // {
+        //     if(i % 2 != 0)
+        //         Console.Write(i+ " ");
+        // }
+
+
+        // int sumOdd = 0;
+        // int sumEven = 0;
+
+        // for (int i = 1; i <= 1000; i++)
+        // {
+        //     if(i % 2 == 0)
+        //        sumEven +=i;
+        //     else
+        //         sumOdd +=i; 
+        // }
+
+        // Console.WriteLine("Sum of the odd numbers :" + sumOdd);     
+        // Console.WriteLine("Sum of even numbers : " + sumEven);
+
+
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            if(i == 4)
+                break;
+            System.Console.WriteLine(i);
+        }
+
+        for (int i = 0; i <10 ; i++)
+        {
+            if(i == 4)
+                continue;
+            System.Console.WriteLine(i);
+        }
+
     }
+
+
 }
